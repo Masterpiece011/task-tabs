6feae0d8abf74e7898050e79c32a180cd7df0eed
Yura Korchagin <yura.korchagin.03@gmail.com>
2024-06-27 13:30:48 +0300

    fixed script

--- js/main.js
+++ js/main.js
@@ -1,23 +1,21 @@
 "use strict"
 
 const allTabsIds = []
-let pickedTabsIds = [1]
+let pickedTabsIds = [0]
 
 const continueBtn = document.querySelector(".js-continue-btn")
 const allTabsElems = document.querySelectorAll(".js-tabs-item")
 const allCurrentTabListElems = document.querySelectorAll(".js-current-tab-list")
 const progressBarElem = document.querySelector(".js-progress-bar")
 
-progressBarElem.style.width = 250 +"px"
-
 function refreshProgressBar(countPickedTabs) {
-    if (countPickedTabs === 3) {
+    if (countPickedTabs > (allTabsIds.length / 2)) {
         progressBarElem.style.backgroundColor = "#ffd43f" 
     }
-    if (countPickedTabs === 4) {
+    if (countPickedTabs === allTabsIds.length) {
         progressBarElem.style.backgroundColor = "#73BE43" 
     }
-    progressBarElem.style.width = countPickedTabs * 250 + "px"
+    progressBarElem.style.width = countPickedTabs * (1000 / allTabsIds.length) + "px"
 }
 
 function isReadyToContinue() {
@@ -27,15 +25,11 @@ function isReadyToContinue() {
     } 
 }
 
-function showCurrentList(id) {
-    allCurrentTabListElems.forEach(list => {
-        if (list.dataset.id == id) {
-            list.classList.add('active')
-        }   
-    })
+function showCurrentList(idActiveTab) {
+    allCurrentTabListElems[idActiveTab].classList.add("active")
 }
 
-function isCurrentTab() {
+function refreshTabs() {
     allTabsElems.forEach(tab => {
         tab.classList.remove("active")
     })
@@ -44,20 +38,32 @@ function isCurrentTab() {
     })
 }
 
-allTabsElems.forEach(tab => {
-    allTabsIds.push(Number(tab.dataset.id));
-    tab.addEventListener("click", () => {
-        if (!pickedTabsIds.includes(Number(tab.dataset.id))) {
-            pickedTabsIds.push(Number(tab.dataset.id))
+function calculateCurrentTabId() {
+    let activeTabId = 0
+    allTabsElems.forEach((tab, i) => {
+        if (tab.classList.contains("active")) {
+            if (!pickedTabsIds.includes(i)) {
+                pickedTabsIds.push(i)
+            }
+            activeTabId = i
         }
-        console.log("pickedTabsIds", pickedTabsIds);
-        refreshProgressBar(pickedTabsIds.length)
-        isCurrentTab()
-        showCurrentList(tab.dataset.id)
-        isReadyToContinue()
+    })
+    return activeTabId
+}
+
+allTabsElems.forEach((tab, tabId) => {
+    allTabsIds.push(tabId)
+    tab.addEventListener("click", () => {
+        refreshTabs()
         tab.classList.add("active")
+        let tabId = calculateCurrentTabId()
+        refreshProgressBar(pickedTabsIds.length)
+        showCurrentList(tabId)
+        isReadyToContinue()
     })
 })
+
+progressBarElem.style.width = (1000 / allTabsIds.length) + "px"
 
 
 
